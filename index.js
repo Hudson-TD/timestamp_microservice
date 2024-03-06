a6a5f4b74b10c354f9d126300d851adf96b91b8c
Tyler Hudson <tylerhudson96@gmail.com>
2024-03-06 22:02:59 +0000

    Initial commit

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/api", (req, res) => {
@@ -24,28 +24,32 @@ app.get("/api", (req, res) => {
 
   let formatedGMT = currentUTC.toUTCString();
 
-  res.json({ unix: currentTimeUnix, utc: formatedGMT })
-
-
-})
+  res.json({ unix: currentTimeUnix, utc: formatedGMT });
+});
 
 app.get("/api/:date", (req, res) => {
   let query = req.params.date;
-  let parsedDate = new Date(query);
+  let parsedDate;
+
+  if (!isNaN(query) && isFinite(query)) {
+    // If the parameter is a valid number, assume it's a Unix timestamp
+    parsedDate = new Date(parseInt(query, 10) * 1000);
+  } else {
+    // If not a number, assume date format and attempt to parse
+    parsedDate = new Date(query);
+  }
 
   if (isNaN(parsedDate)) {
-    res.send({ error: "Invalid Date" })
+    res.send({ error: "Invalid Date" });
   } else {
-    let formatedGMT = parsedDate.toUTCString()
+    let formatedGMT = parsedDate.toUTCString();
     let parsedUnix = parsedDate.getTime() / 1000;
-    res.json({ unix: parsedUnix, utc: formatedGMT })
-  };
-
-})
-
-
+    res.json({ unix: parsedUnix, utc: formatedGMT });
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
+
